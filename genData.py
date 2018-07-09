50afefb5af0b00c3e941bb5ceb60948893806ae3
Nathaniel Grevatt <nqg3ux@virginia.edu>
2018-07-09 18:21:13 -0400

    data.csv generator

---
+++ genData.py
@@ -0,0 +1,20 @@
+from random import shuffle 
+from secrets import choice, randbelow
+
+names = ["james", "mary", "john", "patricia", "robert", 
+        "linda", "michael","barbra", "william", "elizabeth",
+        "david", "jennifer", "richard", "maria", "charles",
+        "susan", "joseph", "margaret", "thomas", "dorothy"]
+
+shuffle(names)
+data = [[i+"@email.com", choice([0,1]), randbelow(100000)] for i in names]
+
+
+import csv
+
+f = open("data.csv", "w")
+
+with f:
+
+    writer = csv.writer(f)
+    writer.writerows(data)
