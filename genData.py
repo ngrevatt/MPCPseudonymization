12bf01757940a082aa7bf46dc336877e9b01c58d
Nathaniel Grevatt <nqg3ux@virginia.edu>
2018-07-19 17:40:03 -0400

    shorter names for data subjects

--- genData.py
+++ genData.py
@@ -1,10 +1,11 @@
 from random import shuffle 
 from secrets import choice, randbelow
 
-names = ["james", "mary", "john", "patricia", "robert", 
-        "linda", "michael","barbra", "william", "elizabeth",
-        "david", "jennifer", "richard", "maria", "charles",
-        "susan", "joseph", "margaret", "thomas", "dorothy"]
+# names under 8 characters? am I forgetting an endstring character?
+names = ["james", "mary", "john", "lisa", "robert", 
+        "linda", "michael","barbra", "william", "helen",
+        "david", "sandra", "richard", "maria", "charles",
+        "susan", "joseph", "carol", "thomas", "dorothy"]
 
 shuffle(names)
 data = [[i+"@email.com", choice([0,1]), randbelow(100000)] for i in names]
